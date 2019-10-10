211801c22afaa961a771e2408902039e635f3f06
stackdump <myork@stackdump.com>
2019-10-10 09:59:01 -0500

    refactor prometheus metrics

--- state/instrumentation.go
+++ state/instrumentation.go
@@ -1,286 +1,90 @@
 package state
 
-import (
-	"github.com/prometheus/client_golang/prometheus"
-)
+import "github.com/FactomProject/factomd/telemetry"
 
 var (
-	// 		Example
-	//stateRandomCounter = prometheus.NewCounter(prometheus.CounterOpts{
-	//	Name: "factomd_state_randomcounter_total",
-	//	Help: "Just a basic counter that can only go up",
-	//})
-	//
+	RegisterPrometheus = telemetry.RegisterPrometheus
+
+	// TODO: refactor to create metrics during initialization
 
 	// Entry Syncing Controller
-	ESMissingQueue = prometheus.NewGauge(prometheus.GaugeOpts{
-		Name: "factomd_state_es_missing_entry_queue",
-		Help: "Number of known missing entries in our queue to find.",
-	})
-	ESMissing = prometheus.NewGauge(prometheus.GaugeOpts{
-		Name: "factomd_state_es_missing_entries",
-		Help: "Number of known missing entries",
-	})
-	ESFound = prometheus.NewGauge(prometheus.GaugeOpts{
-		Name: "factomd_state_es_found_entries",
-		Help: "Number of known missing entries found.",
-	})
-	ESAsking = prometheus.NewGauge(prometheus.GaugeOpts{
-		Name: "factomd_state_es_asking_missing_entries",
-		Help: "Number we are asking for of the known missing entries.",
-	})
-	ESHighestAsking = prometheus.NewGauge(prometheus.GaugeOpts{
-		Name: "factomd_state_es_highest_asking_entries",
-		Help: "Highest entry DBHeight which has has a request made.",
-	})
-	ESHighestMissing = prometheus.NewGauge(prometheus.GaugeOpts{
-		Name: "factomd_state_es_highest_missing_entries",
-		Help: "Highest DBHeight of the entries we know are missing.",
-	})
-	ESFirstMissing = prometheus.NewGauge(prometheus.GaugeOpts{
-		Name: "factomd_state_es_first_missing_entries",
-		Help: "First DBHeight with a missing entry",
-	})
-	ESDBHTComplete = prometheus.NewGauge(prometheus.GaugeOpts{
-		Name: "factomd_state_es_entry_dbheight_complete",
-		Help: "First DBHeight with a missing entry",
-	})
-	ESAvgRequests = prometheus.NewGauge(prometheus.GaugeOpts{
-		Name: "factomd_state_es_average_requests",
-		Help: "Average number of times we have had to request a missing entry",
-	})
-	HighestAck = prometheus.NewGauge(prometheus.GaugeOpts{
-		Name: "factomd_state_highest_ack",
-		Help: "Acknowledgement with the highest directory block height",
-	})
-	HighestKnown = prometheus.NewGauge(prometheus.GaugeOpts{
-		Name: "factomd_state_highest_known",
-		Help: "Highest known block (which can be different than the highest ack)",
-	})
-	HighestSaved = prometheus.NewGauge(prometheus.GaugeOpts{
-		Name: "factomd_state_highest_saved",
-		Help: "Highest saved block to the database",
-	})
-	HighestCompleted = prometheus.NewGauge(prometheus.GaugeOpts{
-		Name: "factomd_state_highest_completed",
-		Help: "Highest completed block, which may or may not be saved to the database",
-	})
+	ESMissingQueue   = telemetry.ESMissingQueue
+	ESMissing        = telemetry.ESMissing
+	ESFound          = telemetry.ESFound
+	ESAsking         = telemetry.ESAsking
+	ESHighestAsking  = telemetry.ESHighestAsking
+	ESHighestMissing = telemetry.ESHighestMissing
+	ESFirstMissing   = telemetry.ESFirstMissing
+	ESDBHTComplete   = telemetry.ESDBHTComplete
+	ESAvgRequests    = telemetry.ESAvgRequests
+	HighestAck       = telemetry.HighestAck
+	HighestKnown     = telemetry.HighestKnown
+	HighestSaved     = telemetry.HighestSaved
+	HighestCompleted = telemetry.HighestCompleted
 
 	// TPS
-	TotalTransactionPerSecond = prometheus.NewGauge(prometheus.GaugeOpts{
-		Name: "factomd_state_txrate_total_tps",
-		Help: "Total transactions over life of node",
-	})
-
-	InstantTransactionPerSecond = prometheus.NewGauge(prometheus.GaugeOpts{
-		Name: "factomd_state_txrate_instant_tps",
-		Help: "Total transactions over life of node weighted for last 3 seconds",
-	})
+	TotalTransactionPerSecond   = telemetry.TotalTransactionPerSecond
+	InstantTransactionPerSecond = telemetry.InstantTransactionPerSecond
 
 	// Torrent
-	stateTorrentSyncingLower = prometheus.NewGauge(prometheus.GaugeOpts{
-		Name: "factomd_state_torrentsync_lower_gauge",
-		Help: "The lower limit of torrent sync",
-	})
-
-	stateTorrentSyncingUpper = prometheus.NewGauge(prometheus.GaugeOpts{
-		Name: "factomd_state_torrentsync_upper_gauge",
-		Help: "The upper limit of torrent sync",
-	})
+	stateTorrentSyncingLower = telemetry.StateTorrentSyncingLower
+	stateTorrentSyncingUpper = telemetry.StateTorrentSyncingUpper
 
 	// Queues
-	CurrentMessageQueueInMsgGeneralVec = prometheus.NewGaugeVec(prometheus.GaugeOpts{
-		Name: "factomd_state_queue_current_general_inmsg_vec",
-		Help: "Instrumenting the current  inmsg queue ",
-	}, []string{"message"})
-
-	TotalMessageQueueInMsgGeneralVec = prometheus.NewGaugeVec(prometheus.GaugeOpts{
-		Name: "factomd_state_queue_total_general_inmsg_vec",
-		Help: "Instrumenting the inmsg queue ",
-	}, []string{"message"})
-
-	CurrentMessageQueueApiGeneralVec = prometheus.NewGaugeVec(prometheus.GaugeOpts{
-		Name: "factomd_state_queue_current_general_api_vec",
-		Help: "Instrumenting the current API queue ",
-	}, []string{"message"})
-
-	TotalMessageQueueApiGeneralVec = prometheus.NewGaugeVec(prometheus.GaugeOpts{
-		Name: "factomd_state_queue_total_general_api_vec",
-		Help: "Instrumenting the API queue ",
-	}, []string{"message"})
-
-	TotalMessageQueueNetOutMsgGeneralVec = prometheus.NewGaugeVec(prometheus.GaugeOpts{
-		Name: "factomd_state_queue_total_general_netoutmsg_vec",
-		Help: "Instrumenting the netoutmsg queue ",
-	}, []string{"message"})
+	CurrentMessageQueueInMsgGeneralVec   = telemetry.CurrentMessageQueueInMsgGeneralVec
+	TotalMessageQueueInMsgGeneralVec     = telemetry.TotalMessageQueueInMsgGeneralVec
+	CurrentMessageQueueApiGeneralVec     = telemetry.CurrentMessageQueueApiGeneralVec
+	TotalMessageQueueApiGeneralVec       = telemetry.TotalMessageQueueApiGeneralVec
+	TotalMessageQueueNetOutMsgGeneralVec = telemetry.TotalMessageQueueNetOutMsgGeneralVec
 
 	// MsgQueue chan
-	TotalMsgQueueInputs = prometheus.NewCounter(prometheus.CounterOpts{
-		Name: "factomd_state_msgqueue_total_inputs",
-		Help: "Tally of total messages gone into MsgQueue (useful for rating)",
-	})
-	TotalMsgQueueOutputs = prometheus.NewCounter(prometheus.CounterOpts{
-		Name: "factomd_state_msgqueue_total_outputs",
-		Help: "Tally of total messages drained out of MsgQueue (useful for rating)",
-	})
+	TotalMsgQueueInputs  = telemetry.TotalMsgQueueInputs
+	TotalMsgQueueOutputs = telemetry.TotalMsgQueueOutputs
 
 	// Holding Queue
-	TotalHoldingQueueInputs = prometheus.NewCounter(prometheus.CounterOpts{
-		Name: "factomd_state_holding_queue_total_inputs",
-		Help: "Tally of total messages gone into Holding (useful for rating)",
-	})
-	TotalHoldingQueueOutputs = prometheus.NewCounter(prometheus.CounterOpts{
-		Name: "factomd_state_holding_queue_total_outputs",
-		Help: "Tally of total messages drained out of Holding (useful for rating)",
-	})
-	TotalHoldingQueueRecycles = prometheus.NewCounter(prometheus.CounterOpts{
-		Name: "factomd_state_holding_queue_total_recycles",
-		Help: "Tally of total messages recycled thru Holding (useful for rating)",
-	})
-	HoldingQueueDBSigInputs = prometheus.NewCounter(prometheus.CounterOpts{
-		Name: "factomd_state_holding_queue_dbsig_inputs",
-		Help: "Tally of DBSig messages gone into Holding (useful for rating)",
-	})
-	HoldingQueueDBSigOutputs = prometheus.NewCounter(prometheus.CounterOpts{
-		Name: "factomd_state_holding_queue_dbsig_outputs",
-		Help: "Tally of DBSig messages drained out of Holding",
-	})
-	HoldingQueueCommitEntryInputs = prometheus.NewCounter(prometheus.CounterOpts{
-		Name: "factomd_state_holding_queue_commitentry_inputs",
-		Help: "Tally of CommitEntry messages gone into Holding (useful for rating)",
-	})
-	HoldingQueueCommitEntryOutputs = prometheus.NewCounter(prometheus.CounterOpts{
-		Name: "factomd_state_holding_queue_commitentry_outputs",
-		Help: "Tally of CommitEntry messages drained out of Holding",
-	})
-	HoldingQueueCommitChainInputs = prometheus.NewCounter(prometheus.CounterOpts{
-		Name: "factomd_state_holding_queue_commitchain_inputs",
-		Help: "Tally of CommitChain messages gone into Holding (useful for rating)",
-	})
-	HoldingQueueCommitChainOutputs = prometheus.NewCounter(prometheus.CounterOpts{
-		Name: "factomd_state_holding_queue_commitchain_outputs",
-		Help: "Tally of CommitChain messages drained out of Holding",
-	})
-	HoldingQueueRevealEntryInputs = prometheus.NewCounter(prometheus.CounterOpts{
-		Name: "factomd_state_holding_queue_revealentry_inputs",
-		Help: "Tally of RevealEntry messages gone into Holding (useful for rating)",
-	})
-	HoldingQueueRevealEntryOutputs = prometheus.NewCounter(prometheus.CounterOpts{
-		Name: "factomd_state_holding_queue_revealentry_outputs",
-		Help: "Tally of RevealEntry messages drained out of Holding",
-	})
+	TotalHoldingQueueInputs        = telemetry.TotalHoldingQueueInputs
+	TotalHoldingQueueOutputs       = telemetry.TotalHoldingQueueOutputs
+	TotalHoldingQueueRecycles      = telemetry.TotalHoldingQueueRecycles
+	HoldingQueueDBSigInputs        = telemetry.HoldingQueueDBSigInputs
+	HoldingQueueDBSigOutputs       = telemetry.HoldingQueueDBSigOutputs
+	HoldingQueueCommitEntryInputs  = telemetry.HoldingQueueCommitEntryInputs
+	HoldingQueueCommitEntryOutputs = telemetry.HoldingQueueCommitEntryOutputs
+	HoldingQueueCommitChainInputs  = telemetry.HoldingQueueCommitChainInputs
+	HoldingQueueCommitChainOutputs = telemetry.HoldingQueueCommitChainOutputs
+	HoldingQueueRevealEntryInputs  = telemetry.HoldingQueueRevealEntryInputs
+	HoldingQueueRevealEntryOutputs = telemetry.HoldingQueueRevealEntryOutputs
 
-	// Acks Queue
-	TotalAcksInputs = prometheus.NewCounter(prometheus.CounterOpts{
-		Name: "factomd_state_acks_total_inputs",
-		Help: "Tally of total messages gone into Acks (useful for rating)",
-	})
-	TotalAcksOutputs = prometheus.NewCounter(prometheus.CounterOpts{
-		Name: "factomd_state_acks_total_outputs",
-		Help: "Tally of total messages drained out of Acks (useful for rating)",
-	})
+	// Acks Queue                          // Acks Queue
+	TotalAcksInputs  = telemetry.TotalAcksInputs
+	TotalAcksOutputs = telemetry.TotalAcksOutputs
 
-	// Commits map
-	TotalCommitsInputs = prometheus.NewCounter(prometheus.CounterOpts{
-		Name: "factomd_state_commits_total_inputs",
-		Help: "Tally of total messages gone into Commits (useful for rating)",
-	})
-	TotalCommitsOutputs = prometheus.NewCounter(prometheus.CounterOpts{
-		Name: "factomd_state_commits_total_outputs",
-		Help: "Tally of total messages drained out of Commits (useful for rating)",
-	})
+	// Commits map                         // Commits map
+	TotalCommitsInputs  = telemetry.TotalCommitsInputs
+	TotalCommitsOutputs = telemetry.TotalCommitsOutputs
 
-	// XReview Queue
-	TotalXReviewQueueInputs = prometheus.NewCounter(prometheus.CounterOpts{
-		Name: "factomd_state_xreview_queue_total_inputs",
-		Help: "Tally of total messages gone into XReview (useful for rating)",
-	})
-	TotalXReviewQueueOutputs = prometheus.NewCounter(prometheus.CounterOpts{
-		Name: "factomd_state_xreview_queue_total_outputs",
-		Help: "Tally of total messages drained out of XReview (useful for rating)",
-	})
+	// XReview Queue                       // XReview Queue
+	TotalXReviewQueueInputs  = telemetry.TotalXReviewQueueInputs
+	TotalXReviewQueueOutputs = telemetry.TotalXReviewQueueOutputs
 
-	// Executions
-	LeaderExecutions = prometheus.NewCounter(prometheus.CounterOpts{
-		Name: "factomd_state_leader_executions",
-		Help: "Tally of total messages executed via LeaderExecute",
-	})
-	FollowerExecutions = prometheus.NewCounter(prometheus.CounterOpts{
-		Name: "factomd_state_follower_executions",
-		Help: "Tally of total messages executed via FollowerExecute",
-	})
-	LeaderEOMExecutions = prometheus.NewCounter(prometheus.CounterOpts{
-		Name: "factomd_state_leader_eom_executions",
-		Help: "Tally of total messages executed via LeaderExecuteEOM",
-	})
-	FollowerEOMExecutions = prometheus.NewCounter(prometheus.CounterOpts{
-		Name: "factomd_state_follower_eom_executions",
-		Help: "Tally of total messages executed via FollowerExecuteEOM",
-	})
-	FollowerMissingMsgExecutions = prometheus.NewCounter(prometheus.CounterOpts{
-		Name: "factomd_state_follower_mm_executions",
-		Help: "Tally of total messages executed via FollowerExecuteMissingMsg",
-	})
+	// Executions                          // Executions
+	LeaderExecutions             = telemetry.LeaderExecutions
+	FollowerExecutions           = telemetry.FollowerExecutions
+	LeaderEOMExecutions          = telemetry.LeaderEOMExecutions
+	FollowerEOMExecutions        = telemetry.FollowerEOMExecutions
+	FollowerMissingMsgExecutions = telemetry.FollowerMissingMsgExecutions
 
-	// ProcessList
-	TotalProcessListInputs = prometheus.NewCounter(prometheus.CounterOpts{
-		Name: "factomd_state_process_list_inputs",
-		Help: "Tally of total messages gone into ProcessLists (useful for rating)",
-	})
-	TotalProcessListProcesses = prometheus.NewCounter(prometheus.CounterOpts{
-		Name: "factomd_state_process_list_processes",
-		Help: "Tally of total messages processed from ProcessLists (useful for rating)",
-	})
-	TotalProcessEOMs = prometheus.NewCounter(prometheus.CounterOpts{
-		Name: "factomd_state_process_eom_processes",
-		Help: "Tally of EOM messages processed from ProcessLists (useful for rating)",
-	})
+	// ProcessList                         // ProcessList
+	TotalProcessListInputs    = telemetry.TotalProcessListInputs
+	TotalProcessListProcesses = telemetry.TotalProcessListProcesses
+	TotalProcessEOMs          = telemetry.TotalProcessEOMs
 
-	// Durations
-	TotalReviewHoldingTime = prometheus.NewGauge(prometheus.GaugeOpts{
-		Name: "factomd_state_review_holding_time",
-		Help: "Time spent in ReviewHolding()",
-	})
-	TotalProcessXReviewTime = prometheus.NewGauge(prometheus.GaugeOpts{
-		Name: "factomd_state_process_xreview_time",
-		Help: "Time spent Processing XReview",
-	})
-	TotalProcessProcChanTime = prometheus.NewGauge(prometheus.GaugeOpts{
-		Name: "factomd_state_process_proc_chan_time",
-		Help: "Time spent Processing Process Chan",
-	})
-	TotalEmptyLoopTime = prometheus.NewGauge(prometheus.GaugeOpts{
-		Name: "factomd_state_empty_loop_time",
-		Help: "Time spent in empty loop",
-	})
-	TotalAckLoopTime = prometheus.NewGauge(prometheus.GaugeOpts{
-		Name: "factomd_state_ack_loop_time",
-		Help: "Time spent in ack loop",
-	})
-	TotalExecuteMsgTime = prometheus.NewGauge(prometheus.GaugeOpts{
-		Name: "factomd_state_execute_msg_time",
-		Help: "Time spent in executeMsg",
-	})
-
-	//		Eom/DBSig delay
-	LeaderSyncAckDelay = prometheus.NewGaugeVec(prometheus.GaugeOpts{
-		Name: "factomd_state_consensus_sync_delay_ack_vec_sec",
-		Help: "Instruments the delay in the time an ack (eom/dbsig only) was signed by a leader, " +
-			"and the time it took our node to receive it in nanoseconds.",
-	}, []string{"leader"})
-
-	LeaderSyncMsgDelay = prometheus.NewGaugeVec(prometheus.GaugeOpts{
-		Name: "factomd_state_consensus_sync_delay_msg_vec_sec",
-		Help: "Instruments the delay in the time a msg (eom/dbsig only) was signed by a leader, " +
-			"and the time it took our node to receive it in nanoseconds.",
-	}, []string{"leader"})
-
-	LeaderSyncAckPairDelay = prometheus.NewGaugeVec(prometheus.GaugeOpts{
-		Name: "factomd_state_consensus_ackpair_delay_vec_sec",
-		Help: "Instruments the delay in the time an ack & msg pair was received. " +
-			"If there is a delay, it means the ack+msg would benefit from being " +
-			"coupled. The delay is measured in seconds.",
-	}, []string{"leader"})
+	// Durations                           // Durations
+	TotalReviewHoldingTime   = telemetry.TotalReviewHoldingTime
+	TotalProcessXReviewTime  = telemetry.TotalProcessXReviewTime
+	TotalProcessProcChanTime = telemetry.TotalProcessProcChanTime
+	TotalEmptyLoopTime       = telemetry.TotalEmptyLoopTime
+	TotalAckLoopTime         = telemetry.TotalAckLoopTime
+	TotalExecuteMsgTime      = telemetry.TotalExecuteMsgTime
 )
 
 var registered bool = false
@@ -372,8 +176,4 @@ func RegisterPrometheus() {
 	prometheus.MustRegister(TotalEmptyLoopTime)
 	prometheus.MustRegister(TotalAckLoopTime)
 	prometheus.MustRegister(TotalExecuteMsgTime)
-	//		Delays
-	prometheus.MustRegister(LeaderSyncMsgDelay)
-	prometheus.MustRegister(LeaderSyncAckDelay)
-	prometheus.MustRegister(LeaderSyncAckPairDelay)
 }
