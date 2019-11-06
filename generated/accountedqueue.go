f487859d95443b8cfecf1889983cbf154f6959a8
clay douglass <clay@factom.com>
2019-11-06 08:46:57 -0600

    updated accountedqueue templates to work for non nilable types.

--- generated/accountedqueue.go
+++ generated/accountedqueue.go
@@ -55,7 +55,7 @@ func (q Queue_IMsg) Enqueue(m interfaces.IMsg) {
 }
 
 // Enqueue adds item to channel and instruments based on
-// returns true it it enqueues the data
+// returns true if it enqueues the data
 func (q Queue_IMsg) EnqueueNonBlocking(m interfaces.IMsg) bool {
 	select {
 	case q.Channel <- m:
@@ -68,6 +68,7 @@ func (q Queue_IMsg) EnqueueNonBlocking(m interfaces.IMsg) bool {
 }
 
 // Dequeue removes an item from channel
+<<<<<<< HEAD
 // Returns nil if nothing in // queue
 func (q Queue_IMsg) Dequeue() interfaces.IMsg {
 	select {
@@ -136,12 +137,40 @@ func (q Queue_int) EnqueueNonBlocking(m int) bool {
 		q.TotalMetric().Inc()
 		q.Metric().Inc()
 		return true
+=======
+// Return value and true if open or zero and false if closed
+func (q Queue_IMsg) DequeueFlag() (rval interfaces.IMsg, open bool) {
+	v, open := <-q.Channel
+	if open {
+		q.Metric().Dec()
+	}
+	return v, open
+}
+
+// Dequeue removes an item from channel
+func (q Queue_IMsg) Dequeue() (rval interfaces.IMsg) {
+	v, _ := q.DequeueFlag()
+	return v
+}
+
+// Dequeue removes an item from channel
+// Returns zero value if nothing in queue or closed
+// Returns open and empty flags
+func (q Queue_IMsg) DequeueNonBlockingFlags() (rval interfaces.IMsg, open bool, empty bool) {
+	select {
+	case rval, open = <-q.Channel:
+		if open {
+			q.Metric().Dec()
+		}
+		return rval, open, !open // if it is closed it is empty
+>>>>>>> updated accountedqueue templates to work for non nilable types.
 	default:
 		return false
 	}
 }
 
 // Dequeue removes an item from channel
+<<<<<<< HEAD
 // Returns nil if nothing in // queue
 func (q Queue_int) Dequeue() int {
 	select {
@@ -158,6 +187,12 @@ func (q Queue_int) BlockingDequeue() int {
 	v := <-q.Channel
 	q.Metric().Dec()
 	return v
+=======
+// Returns zero value if nothing in queue or closed
+func (q Queue_IMsg) DequeueNonBlocking() (rval interfaces.IMsg) {
+	rval, _, _ = q.DequeueNonBlockingFlags()
+	return rval
+>>>>>>> updated accountedqueue templates to work for non nilable types.
 }
 
 // End accountedqueue generated go code
